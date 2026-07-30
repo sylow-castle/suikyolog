a298bf75c6cd46a4b2194cc9cc8a0dd53c43dfa7
s.castle <s.castle@sylow-castle.work>
2026-07-30 22:53:21 +0900

    [update]検証用コードが混入していた

--- src/node/SyncFileWriter.js
+++ src/node/SyncFileWriter.js
@@ -12,8 +12,8 @@ export class SyncFileWriter extends Writer {
   constructor(options = {}) {
     super(options);
     this.#path = options.path;
-    //    const flags = fs.constants.O_WRONLY | fs.constants.O_CREAT | fs.constants.O_APPEND;
-    const flags = fs.constants.O_WRONLY | fs.constants.O_CREAT | fs.constants.O_APPEND | fs.constants.O_DSYNC;
+    const flags = fs.constants.O_WRONLY | fs.constants.O_CREAT | fs.constants.O_APPEND;
+    //const flags = fs.constants.O_WRONLY | fs.constants.O_CREAT | fs.constants.O_APPEND | fs.constants.O_DSYNC;
     this.#fd = fs.openSync(this.#path, flags);
   }
 
